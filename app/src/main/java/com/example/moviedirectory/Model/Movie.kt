b968eeaaeda1ce9f4dbb0035dd09c4f624c6216f
Soumyadeep Pradhan <soumyadeeppradhan@gmail.com>
2021-02-02 16:33:18 +0530

    Final Commit

--- app/src/main/java/com/example/moviedirectory/Model/Movie.kt
+++ app/src/main/java/com/example/moviedirectory/Model/Movie.kt
@@ -1,58 +1,19 @@
-package com.example.moviedirectory.Model;
+package com.example.moviedirectory.Model
 
-import java.io.Serializable;
+import java.io.Serializable
 
-public class Movie implements Serializable {
-    private static final long id = 1L;
-    private String title,year,imdbID,poster, movieType,backGround;
-
-    public Movie(){
+class Movie : Serializable {
+    var title: String? = null
+    var year: String? = null
+    var imdbID: String? = null
+    var poster: String? = null
+    var movieType: String? = null
+    private var backGround: String? = null
+    fun setBackGround(backGround: String?) {
+        this.backGround = backGround
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-
-    public String getImdbID() {
-        return imdbID;
-    }
-
-    public void setImdbID(String imdbID) {
-        this.imdbID = imdbID;
-    }
-
-    public String getPoster() {
-        return poster;
-    }
-
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
-
-
-    public String getMovieType() {
-        return movieType;
-    }
-
-    public void setMovieType(String movieType) {
-        this.movieType = movieType;
-    }
-
-    public void setBackGround(String backGround) {
-        this.backGround = backGround;
+    companion object {
+        private const val id = 1L
     }
 }
